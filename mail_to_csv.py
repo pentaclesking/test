afeb74a0f772d8994408821c057bfb40b85d2a17
User_new <it.simpl@gmail.com>
2022-05-23 08:59:42 +0300

    init2

--- mail_to_csv.py
+++ mail_to_csv.py
@@ -166,6 +166,7 @@ except Exception as e:
 
 print('eeee')
 print('eeee')
+print('eeee')
 # from_date = datetime.date.today() - datetime.timedelta(constants.days_ago)
 # save_attachments_only('Zoho2', from_date)
 
